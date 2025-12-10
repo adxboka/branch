b09788c00068c9c7cdaeb88bc2173ead9fca3d3b
aditya347711-bit <aditya347711@gmail.com>
2025-12-11 00:51:48 +0530

    Update app.js and style.css

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// #this is app 
+const btn = document.querySelector("#btn");
+const p = document.querySelector("p");
+
+btn.addEventListener("click", () => {
+  console.log("hey");
+
+  p.style.backgroundColor = "black";
+  p.style.color = "white";
+});
+
+console.log(btn);
